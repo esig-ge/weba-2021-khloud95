c3e661865ccc50ffbf1cb17d79d8c36328f0000d
khloud95 <khloud.mhmd@eduge.ch>
2021-11-27 18:04:58 +0100

    edit project

--- amatha/settings.py
+++ amatha/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 #STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles/")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "staticfiles/")
+# ]
 MEDIA_URL = '/img/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/img/')
 # Default primary key field type
--- amathaTest/README.md
+++
@@ -1,6 +0,0 @@
-Bonjour !
-
-Ici le dossier total concernant le rendu du site AMATHA v2.1. 
-
-
-Bonne visite

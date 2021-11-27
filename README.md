6331b13fd7d6f8df6035c5b13db119c7eccee620
khloud95 <khloud.mhmd@eduge.ch>
2021-11-27 15:37:09 +0100

    Revert "push amatha project"
    This reverts commit d5423886a822f53ba50443169001b7e2b249da3f.

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-Bonjour !
-
-Ici le dossier total concernant le rendu du site AMATHA v2.1. 
-
-
-Bonne visite
+# template-classroom-vide
--- paiement/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PaiementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'paiement'

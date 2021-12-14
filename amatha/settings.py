d1537c855a5e639320e66dc16e54251223fb83dc
khloud95 <84767320+khloud95@users.noreply.github.com>
2021-12-15 00:10:08 +0100

    Revert "edit settings"
    This reverts commit 3f836a0f2bc3a798790804ff8b4e44ffa093043b.

--- amatha/settings.py
+++ amatha/settings.py
@@ -118,16 +118,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+#STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'staticfiles')
+#     os.path.join(BASE_DIR, "staticfiles/")
 # ]
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
-
-
-
 MEDIA_URL = '/img/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/img/')
 # Default primary key field type
